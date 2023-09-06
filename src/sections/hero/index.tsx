d98d4cd87a6db42727cc53e2c69de18f84f17a01
Vedant Chainani <vedantchainani1084@gmail.com>
2023-09-07 03:17:39 +0530

    add hero section

---
+++ src/sections/hero/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'framer-motion';
+
+export default function MultiLayerParallax() {
+	const ref = React.useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ['start start', 'end start'],
+	});
+	const backgroundY = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+	const textY = useTransform(scrollYProgress, [0, 1], ['0%', '90%']);
+
+	return (
+		<div
+			ref={ref}
+			className='relative flex min-h-[130vh] w-full snap-start justify-center overflow-y-hidden'
+		>
+			<motion.div
+				style={{ y: textY }}
+				className={`relative z-10 mt-[16rem] px-4`}
+			>
+				<div className='font-adieuRegular flex flex-col items-center gap-4 text-center font-black text-[#404655]'>
+					<div className='text-[1rem] sm:text-lg'>
+						IDEATE. INNOVATE. INCUBATE.
+					</div>
+					<div className='text-[1.75rem] sm:text-5xl lg:text-6xl xl:text-8xl'>
+						Entrepreneurship Cell
+					</div>
+					<div className='text-[1.75rem] sm:text-5xl lg:text-6xl xl:text-7xl'>
+						NIT Agartala
+					</div>
+				</div>
+			</motion.div>
+
+			<motion.div
+				className='absolute inset-0 z-0'
+				style={{
+					y: backgroundY,
+				}}
+			>
+				<Image
+					src='/hero.webp'
+					alt='Hero Image'
+					width={1920}
+					height={1080}
+					className='bottom-0 h-screen w-full object-cover'
+				/>
+			</motion.div>
+		</div>
+	);
+}
