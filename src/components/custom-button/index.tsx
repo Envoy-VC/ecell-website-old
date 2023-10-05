f4a9ecd1516630cc4464805c21752b7813798535
Vedant Chainani <vedantchainani1084@gmail.com>
2023-10-06 00:53:59 +0530

    update button styles

--- src/components/custom-button/index.tsx
+++ src/components/custom-button/index.tsx
@@ -6,14 +6,16 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 const Button = ({ children }: ButtonProps) => {
 	return (
 		<motion.button
-			className='group relative w-fit overflow-hidden border-[2px] border-primary bg-white px-8 py-2'
+			className='button-gradient group relative w-fit overflow-hidden'
 			whileTap={{ scale: 1 }}
 			whileHover={{ scale: 1.05 }}
 		>
-			<div className='button-gradient absolute inset-0 translate-x-[-100%] translate-y-[100%] rotate-45 scale-[260%] rounded-full transition-transform duration-300 group-hover:translate-x-[0%] group-hover:translate-y-[0%]' />
-			<h3 className='relative z-10 font-adieuRegular text-lg font-medium text-primary duration-300 group-hover:text-white'>
-				{children}
-			</h3>
+			<div className='m-[2px] bg-white px-8 py-2'>
+				<div className='button-gradient absolute inset-0 translate-x-[-100%] translate-y-[100%] rotate-45 scale-[260%] rounded-full transition-transform duration-[400ms] group-hover:translate-x-[0%] group-hover:translate-y-[0%]' />
+				<h3 className='relative z-10 font-adieuRegular text-lg font-medium text-primary duration-300 group-hover:text-white'>
+					{children}
+				</h3>
+			</div>
 		</motion.button>
 	);
 };
