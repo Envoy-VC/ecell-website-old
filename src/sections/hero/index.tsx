35391f9c971e2b508ddd6d8d225d79e85c144a4f
Vedant Chainani <vedantchainani1084@gmail.com>
2023-10-31 20:07:44 +0530

    add explore button

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 
 import BackgroundImage from '../../../public/bg-illustration.jpg';
+import { HiCursorClick } from 'react-icons/hi';
 
 // Variants
 import { textVariants } from '~/utils/framer';
@@ -12,7 +14,7 @@ import Typewriter from 'typewriter-effect';
 export default function MultiLayerParallax() {
 	return (
 		<div className='relative flex h-screen w-full snap-start justify-center overflow-hidden'>
-			<motion.div className={`relative z-10 mt-[10rem] px-4`}>
+			<motion.div className={`relative z-10 mt-[6rem] px-4`}>
 				<div className='flex flex-col items-center gap-4 text-center font-adieuRegular font-black tracking-wider text-textPrimary'>
 					<motion.div
 						className='sm:text-xl'
@@ -57,6 +59,16 @@ export default function MultiLayerParallax() {
 						<div className='text-[2rem] sm:text-4xl lg:text-5xl xl:text-6xl'>
 							NIT Agartala
 						</div>
+						<Link href='#about'>
+							<motion.button
+								whileHover={{ scale: 1.025 }}
+								whileTap={{ scale: 0.99 }}
+								className='mx-auto mt-8 flex w-fit flex-row items-center gap-2 rounded-3xl border-2 border-black px-4 py-2 text-lg shadow-sm'
+							>
+								Explore
+								<HiCursorClick />
+							</motion.button>
+						</Link>
 					</motion.div>
 				</div>
 			</motion.div>
